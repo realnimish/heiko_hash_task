449fe8b784e8fe72be87e041207af9af31691236
Nimish Agrawal <realnimish@gmail.com>
2024-11-10 18:07:57 +0530

    chore: add benchmark for `aggregate_hashes_by_parts`

--- benches/aggregator_benchmark.rs
+++ benches/aggregator_benchmark.rs
@@ -1,11 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use heiko_hash_task::{aggregator::aggregate_hashes, helpers::generate_random_hashes};
+use heiko_hash_task::{aggregator, helpers::generate_random_hashes};
 
 fn benchmark_aggregate_hashes(c: &mut Criterion) {
     let hashes = generate_random_hashes();
 
     c.bench_function("aggreagete hashes", |b| {
-        b.iter(|| aggregate_hashes(black_box(&hashes)))
+        b.iter(|| aggregator::aggregate_hashes(black_box(&hashes)))
+    });
+
+    c.bench_function("aggreagete hashes by parts", |b| {
+        b.iter(|| aggregator::aggregate_hashes_by_parts(black_box(&hashes)))
     });
 }
 
