124e080b5ccb899f5b1fcceeddff724a1409240d
Nimish Agrawal <realnimish@gmail.com>
2024-11-10 17:23:04 +0530

    test: add test for `aggregate_hashes_by_parts`

--- src/tests/test_aggregate_hashes.rs
+++ src/tests/test_aggregate_hashes.rs
@@ -20,4 +20,20 @@ mod tests {
             "Aggregating multiple hashes should return a correct hash sum"
         );
     }
+
+    #[test]
+    fn test_aggregate_by_parts_returns_correct_hash_sum() {
+        let hashes = helpers::generate_random_hashes();
+        let res = aggregator::aggregate_hashes_by_parts(&hashes);
+
+        let mut expected = [0u64; aggregator::HASH_LENGTH_U64];
+        for i in 0..hashes.len() {
+            expected = helpers::add_hashes(&expected, &hashes[i]);
+        }
+
+        assert_eq!(
+            res, expected,
+            "Aggregating multiple hashes should return a correct hash sum"
+        );
+    }
 }
